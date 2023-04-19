eaf01bc6371550a76e0bd0da640f16b4e907f6cd
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-04-19 20:35:08 -0300

    positions characters

--- script.js
+++ script.js
@@ -20,96 +20,54 @@ button.appendChild(dannyButton);
 var robot = new Image (100, 100);
 robot.src = 'images/robot.png';
 robot.className = 'robot'
+robot.style.left = 'var(--position0)';
 /* main.appendChild(robot); */ //add the image at the end of main
 button.before(robot);   //add the image before button, the firt element inside main
 
 var danny = new Image (100, 100);
 danny.src = 'images/danny.png';
 danny.className = 'danny'
+danny.style.left = 'var(--position0)';
 button.before(danny);
 
 
-//FUNCTIONS
+//Variables
 let playerSelection;
-//PLAYROUND:
-function playRound (inputChoice) {
-            
-    playerSelection = inputChoice;
-    computerSelection = getComputerSelection();
-    console.log('You choose: ' + playerSelection);      
-    console.log('Your oponent choose: ' + computerSelection);
+let computerSelection;
+let scoreReport = [];
+let winner;
+let positionDanny;
+let positionRobot;
+let positionWinner;
+const options = ['rock', 'paper', 'knife'];
 
-    switch (playerSelection) {
-    case "Rock":
-        if (computerSelection === 'Paper') {
-            console.log('You Lose! Paper beats Rock');  
-            return 'Computer';
-        } else if (computerSelection === 'Rock') {
-            console.log('Tied! Rock doesn`t beats Rock');  
-            return 'Tied';
-            
-        } else if (computerSelection === 'Knife'){
-            console.log('You Win! Rock beats Knife');  
-            return 'Player';
-        }
-        break;
-    case "Paper":
-        if (computerSelection === 'Knife') {
-            console.log('You Lose! Knife beats Paper');  
-            return 'Computer';
-        } else if (computerSelection === 'Paper') {
-            console.log('Tied! Paper doesn`t beats Paper');  
-            return 'Tied';
-        } else if (computerSelection === 'Rock'){
-            console.log('You Win! Paper beats Rock');  
-            return 'Player';
-        }
-        break
-    case "Knife":
-        if (computerSelection === 'Rock') {
-            console.log('You Lose! Rock beats Knife');  
-            return 'Computer';
-        } else if (computerSelection === 'Knife') {
-            console.log('Tied! Knife doesn`t beats Knife');  
-            return 'Tied';
-        } else if (computerSelection === 'Paper'){
-            console.log('You Win! Knife beats Paper');  
-            return 'Player';
-        }   
-        break;
-    }
-    
-}
+//Function to show instructions and weapons
+function start() {
+    button.style.display ='none';
+    startText.style.display ='flex';
+    startText.style.flexDirection ='column';
+    positionDanny = danny.style.left;
+    positionRobot = robot.style.left;
+} 
 
+//After instructions, function to show weapons choices
 function show () {
+    //Hide text and button, show choices buttons
+    choices.style.display ='inherit'; 
     instructionsText.style.display ='none';
-    choices.style.display ='inherit';
     startButton.style.display ='none';
-    const chooseYourWeapon = document.createElement ('h1');
+    
+    //title 'Choose your weapon' before choices button
+    const chooseYourWeapon = document.createElement ('h1'); 
     chooseYourWeapon.textContent = 'Choose your weapon:';
     choices.before(chooseYourWeapon);
-    var inputChoice = document.getElementById("btnChoice").value;
-    alert(inputChoice);
-    game(inputChoice);
 }
 
-//GAME FUNCTION
-function game (inputChoice) {
-    let scoreReport = [];
-    let winner;
-   
-    console.warn('Starting the game!'); //lixo
+function theEnd () {
 
-    for (let index = 0; index < 5; index++) {
-        console.warn('Round ' + (index+1));
-        scoreReport[index] = [('Round: ' + (index+1)), 'Winner: ' + playRound(inputChoice)];
-    }
-    console.log(scoreReport);
 }
 
-//COMPUTER ROLL:
-const options = ['Rock', 'Paper', 'Knife'];
-let computerSelection;
+//COMPUTER - random roll function:
 function getComputerSelection() {
     //Math.random = randomic number: 0.65436241685
     //* options.length = * 3 positions inside array
@@ -117,21 +75,115 @@ function getComputerSelection() {
     return computerSelection = options[Math.floor(Math.random() * options.length)];
 }
 
+function nextPosition () {
+    if (winner === 'robot') {positionWinner = positionRobot};
+    if (winner === 'danny') {positionWinner = positionDanny};
+    switch (positionWinner) {
+        case "var(--position0)":
+            positionWinner = "var(--position1)";
+            return positionWinner;
+        case "var(--position1)":
+            positionWinner = "var(--position2)";
+            return positionWinner;
+        case "var(--position2)":
+            positionWinner = "var(--position3)";
+            return positionWinner;
+        case "var(--position3)":
+            positionWinner = "var(--position4)";
+            return positionWinner;
+        case "var(--position4)": //and fulano is winner
+            console.log('YOU DIT IT!');
+            return theEnd('danny');
+    }
+}
 
-//Function when the input is text, we changed for button
-/* function getPlayerSelection() {
-    playerSelection = prompt("Choose between: Rock, Paper and Knife!");
-    const firstLetter = playerSelection.charAt(0) //Capitalize only first letter
-    const firstLetterCap = firstLetter.toUpperCase()
-    const remainingLetters = playerSelection.slice(1)
-    const remainingLettersLow = remainingLetters.toLowerCase()
-    playerSelection = firstLetterCap + remainingLettersLow
-    return playerSelection;
-} */
 
-//Function to show instructions and weapons
-function start() {
-    button.style.display ='none';
-    startText.style.display ='flex';
-    startText.style.flexDirection ='column';
-} 
+
+//PLAYROUND:
+function playRound (input) {
+    playerSelection = input;           
+    computerSelection = getComputerSelection();
+    console.warn('partida nova');
+    console.log('Danny choose: ' + playerSelection);      
+    
+    //This was a solution to avoid tied
+    //Maybe is not fair enough with the player
+    //The computer have other chance to play (but it is honest, so worked!)
+    if(playerSelection === computerSelection) {
+        computerSelection = getComputerSelection();
+    }
+    console.log('Robot choose: ' + computerSelection);
+    
+    switch (playerSelection) {
+    case "rock":
+        if (computerSelection === 'paper') {
+            console.log('You Lose! Paper beats Rock');  
+            winner = 'robot';
+
+            robot.style.left = nextPosition(winner);
+            positionRobot = robot.style.left;
+
+            robot.style.transitionDuration = '1s';
+            return positionRobot;
+        /* } else if (computerSelection === 'rock') {
+            console.log('Tied! Rock doesn`t beats Rock');   */
+        } else if (computerSelection === 'knife'){
+            console.log('You Win! Rock beats Knife');  
+            winner = 'danny';
+
+            danny.style.left = nextPosition(winner);
+            positionDanny = danny.style.left;
+
+            danny.style.transitionDuration = '1s';
+            return positionDanny;
+        }
+        break;
+    case "paper":
+        if (computerSelection === 'knife') {
+            console.log('You Lose! Knife beats Paper'); 
+            winner = 'robot';
+
+            robot.style.left = nextPosition(winner);
+            positionRobot = robot.style.left;
+
+            robot.style.transitionDuration = '1s';
+            return positionRobot;
+        /* } else if (computerSelection === 'paper') {
+            console.log('Tied! Paper doesn`t beats Paper');   */
+        } else if (computerSelection === 'rock'){
+            console.log('You Win! Paper beats Rock');  
+            winner = 'danny';
+
+            danny.style.left = nextPosition(winner);
+            positionDanny = danny.style.left;
+
+            danny.style.transitionDuration = '1s';
+            return positionDanny;
+        }
+        break
+    case "knife":
+        if (computerSelection === 'rock') {
+            console.log('You Lose! Rock beats Knife');  
+            winner = 'robot';
+
+            robot.style.left = nextPosition(winner);
+            positionRobot = robot.style.left;
+
+            robot.style.transitionDuration = '1s';
+            return positionRobot;
+        /* } else if (computerSelection === 'knife') {
+            console.log('Tied! Knife doesn`t beats Knife');  */ 
+        } else if (computerSelection === 'paper'){
+            console.log('You Win! Knife beats Paper');  
+            winner = 'danny';
+
+            danny.style.left = nextPosition(winner);
+            positionDanny = danny.style.left;
+
+            danny.style.transitionDuration = '1s';
+            return positionDanny;
+        }   
+        break;
+    }
+    
+}
